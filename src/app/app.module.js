eb37a6505d31f9721dede49d55648933cfb927f3
pikipiki <franky.kp@gmail.com>
2018-04-24 01:52:17 +0200

    add: list of countries in container

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -12,4 +12,8 @@ export const AppModule = angular
     'app', 
     AppComponent
   )
+  .run(($http) => {
+    'ngInject';
+    $http.defaults.headers.common['X-Mashape-Key'] = 'l5eMXwY6d3mshmvnljsx6GVH9YWxp1IsKhsjsnSAZ5yXpYiGRl'
+  })
   .name;
